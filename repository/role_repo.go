904e30a13b9791fb4408d7f8feca312a46bbb97a
khangdx <khang1998@gmail.com>
2022-12-20 09:52:14 +0700

    Add role_repo

--- repository/role_repo.go
+++ repository/role_repo.go
@@ -1,1 +1,21 @@
 package repository
+
+import  (
+	"final_project/models"
+)
+
+func CreateRole(role models.Role){
+
+}
+
+func UpdateRole(condition models.Condition, role models.Role) {
+
+}
+
+func DeleteRole(condition models.Condition) {
+
+}
+
+func ReadRole(condition models.Condition) {
+	
+}
